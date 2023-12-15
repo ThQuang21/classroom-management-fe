ea0c566acadef84ec4d2c45e226b89f9b21009e6
vuhoanganh6401 <vuhoanganh6401@gmail.com>
2023-12-16 03:45:30 +0700

    Grade board management

--- src/page/Grades/GradeManagement/GradeManagementTeacherView.js
+++ src/page/Grades/GradeManagement/GradeManagementTeacherView.js
@@ -6,22 +6,35 @@ import {
   Grid,
   VirtualTable,
   TableHeaderRow,
-  TableFixedColumns,
+  TableFixedColumns, TableEditColumn,
 } from '@devexpress/dx-react-grid-material-ui';
-
+import {
+  Plugin, Template, TemplateConnector, TemplatePlaceholder,
+} from '@devexpress/dx-react-core'
+import { EditingState } from '@devexpress/dx-react-grid';
 import {useEffect, useState} from "react";
 import HandleImportExportStudent from "./ImportFile/HandleImportExportStudent";
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
 import GradeService from "../../../services/grade.service";
 import Container from "@mui/material/Container";
-import {LinearProgress} from "@mui/material";
+import {IconButton, LinearProgress} from "@mui/material";
 import ClassService from "../../../services/class.service";
+import TextField from '@mui/material/TextField';
+import FormGroup from '@mui/material/FormGroup';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import MuiGrid from '@mui/material/Grid';
+import Toolbar from "@mui/material/Toolbar";
+import AppBar from "@mui/material/AppBar";
+import CloseIcon from "@mui/icons-material/Close";
+import TableGrade from "./TableGrade";
 
 export default function GradeManagementTeacherView() {
   const classCode = window.location.pathname.split('/').pop(); // Extract classCode from the URL
-  const [rowData, setRowData] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
 
   const [alertProps, setAlertProps] = useState({
     open: false,
@@ -45,115 +58,19 @@ export default function GradeManagementTeacherView() {
     }, 6000);
   };
 
-  const [columns, setColumns] = useState([]);
-  const [tableColumnExtensions, setTableColumnExtensions] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-
-      if (classCode) {
-        await GradeService.getGradesByClassCode({ classCode: classCode})
-          .then((data) => {
-            console.log(data.data.data);
-            setRowData(data.data.data)
-
-          }, (error) => {
-            console.log(error)
-            showAlert(error.response.data.error.message || 'An unexpected error occurred. Please try again later.', 'error');
-          })
-        ;
-
-        await ClassService.getGradeCompositionByClassCode({ classCode: classCode})
-          .then((data) => {
-            setLoading(false);
-
-            const newGrades = data.data.data.gradeCompositions.map((grade, index) => ({
-              name: grade.name
-            }));
-
-            var column = [
-              { name: 'fullName', title: 'Fulll Name' },
-              { name: 'studentId', title: 'Student ID' },
-            ];
-            var tableColumnExtension = [
-              { columnName: 'fullName', width: 180 }, //fullName
-              { columnName: 'studentId', width: 100 }, //studentId
-
-              // { columnName: 'amount', align: 'right', width: 140 }, //total
-            ];
-            newGrades.forEach((gradeComposition) => {
-              column.push({
-                name: gradeComposition.name,
-                title: gradeComposition.name
-              })
-              tableColumnExtension.push({
-                columnName: gradeComposition.name,
-                align: 'center'
-              })
-            })
-            setColumns(column)
-            setTableColumnExtensions(tableColumnExtension)
-
-            setLoading(false);
-          }, (error) => {
-            showAlert(error.response.data.error.message || 'An unexpected error occurred. Please try again later.', 'error');
-          })
-        ;
-      }
-    };
-
-    const msgDialog = localStorage.getItem('msgDialog');
-    const msgDialogSuccess = localStorage.getItem('msgDialogSuccess');
-
-    if (msgDialog) {
-      showAlert(msgDialog, 'error');
-      localStorage.removeItem('msgDialog')
-    }
-    if (msgDialogSuccess) {
-      showAlert(msgDialogSuccess, 'success');
-      localStorage.removeItem('msgDialogSuccess')
-    }
-    fetchData();
-    // eslint-disable-next-line
-  }, []);
-
-  const [leftColumns] = useState(['fullName', 'studentId']);
-  // const [rightColumns] = useState(['amount']);
-
   const handleReloadTable = async () => {
-    setLoading(true);
+    // setLoading(true);
     await GradeService.getGradesByClassCode({ classCode: classCode})
       .then((data) => {
         console.log(data.data.data);
-        setRowData(data.data.data)
-        setLoading(false)
+        // setRows(data.data.data)
+        // setLoading(false)
       }, (error) => {
         console.log(error)
         showAlert(error.response.data.error.message || 'An unexpected error occurred. Please try again later.', 'error');
       })
     ;
   };
-
-  if (loading) {
-    return (
-      <>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <LinearProgress  />
-        </Container>
-        <Snackbar
-          open={alertProps.open}
-          autoHideDuration={4000}
-          onClose={handleAlertClose}
-          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        >
-          <Alert onClose={handleAlertClose} severity={alertProps.severity} sx={{ width: '100%' }}>
-            {alertProps.message}
-          </Alert>
-        </Snackbar>
-      </>
-
-    )
-  }
 
   return (
     <>
@@ -167,19 +84,7 @@ export default function GradeManagementTeacherView() {
       </Paper>
 
       <Paper sx={{ width: '100%', overflow: 'hidden', marginTop: '15px'}}>
-        <Grid
-          rows={rowData}
-          columns={columns}
-        >
-          <VirtualTable
-            columnExtensions={tableColumnExtensions}
-          />
-          <TableHeaderRow />
-          <TableFixedColumns
-            leftColumns={leftColumns}
-            // rightColumns={rightColumns}
-          />
-        </Grid>
+       <TableGrade/>
 
         <Snackbar
           open={alertProps.open}
