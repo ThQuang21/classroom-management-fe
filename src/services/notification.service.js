13c4d60ddd494124597edced714d1518a7322c74
LuisVu25 <vdhung19@vp.fitus.edu.vn>
2023-12-28 13:18:08 +0700

    Create notification

--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -1,7 +1,7 @@
 import http from "./http-config";
 
-// const API_URL = "https://classroom-management-be.vercel.app/";
-const API_URL = "http://localhost:3000/";
+const API_URL = "https://classroom-management-be.vercel.app/";
+// const API_URL = "http://localhost:3000/";
 
 const createNotification = async ({ senderId, receiverIds, classCode, type, message }) => {
   return await http.post(API_URL + "notifications", {
